f40e1dbd88b5d23f8c54af42be81d4dd970bd4e5
Julian Hoffmann <wi21b050@technikum.at>
2022-06-21 17:25:00 +0200

    Initial commit

---
+++ src/main/java/at/technikum/examslm/ExamSlmApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.examslm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamSlmApplication.class, args);
+    }
+
+}
